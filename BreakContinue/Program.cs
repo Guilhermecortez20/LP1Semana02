1b34e0c9e9f920dfc6c357114fb702acd0cb6ccf
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-27 23:59:27 +0000

    Excluído número 10 da impressão usando continue

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números de 1 a 20 (exceto 10):");
+
+        for (int i = 1; i <= 20; i++)
+        {
+            if (i == 10)
+            {
+                continue; // Pula a iteração quando i for 10
+            }
+
+            Console.WriteLine(i);
+        }
+    }
+}
+
