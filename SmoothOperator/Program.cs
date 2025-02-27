475ce25a13ec230543af5065c3a431441b35f137
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-27 22:47:18 +0000

    Second commit

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insere número inteiro: ");
+        int input;
+        
+        // Verificar se a entrada é um número válido
+        if (int.TryParse(Console.ReadLine(), out input))
+        {
+            // Conversão para sbyte
+            sbyte n = (sbyte)input;
+
+            // Mostrar decremento e incremento
+            Console.WriteLine((sbyte)(n - 1));
+            Console.WriteLine((sbyte)(n + 1));
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida! Insira um número inteiro.");
+        }
+    }
+}
+
+
