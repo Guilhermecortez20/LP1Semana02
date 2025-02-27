db8539160b68b7297532095dcbf7ea47eaedce1a
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-27 23:28:22 +0000

    Criado projeto Cilindro na solução LP1Semana02

---
+++ Cilindro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Insira a altura do cilindro: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Insira o raio do cilindro: ");
+        double raio = Convert.ToDouble(Console.ReadLine());
+
+       
+        double volume = Math.PI * Math.Pow(raio, 2) * altura;
+        double areaSuperficie = 2 * Math.PI * raio * (raio + altura);
+
+        
+        Console.WriteLine($"Volume: {volume:F3}");
+        Console.WriteLine($"Área de superfície: {areaSuperficie:F3}");
+    }
+}
+
