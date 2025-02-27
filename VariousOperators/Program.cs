dc18650688d3ca08631be9f8f964cd13d1f40e13
Guilhermecortez20 <Guilhermecortez445@gmail.com>
2025-02-27 23:06:34 +0000

    Adicionado projeto VariousOperators

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insere número inteiro não-negativo: ");
+        int input;
+
+        
+        if (int.TryParse(Console.ReadLine(), out input) && input >= 0 && input <= 255)
+        {
+            
+            byte n = (byte)input;
+
+           
+            Console.WriteLine(n / 2);       
+            Console.WriteLine(n << 3);      
+            Console.WriteLine(n ^ 6);       
+            Console.WriteLine(n > 10);      
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida! Insira um número entre 0 e 255.");
+        }
+    }
+}
+
